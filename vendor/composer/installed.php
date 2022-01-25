f9e5657c57959c7770f1257d01f0570adaa20b03
danyu <kasyanova.b@effective-group.ru>
2022-01-25 08:56:16 +0300

    Обновление

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'prpject',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b3e4f3ad7361a44456524e2e6a7ec17e923ee1af',
+        'name' => 'kasyanovab/test-2',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'kasyanovab/test-1' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kasyanovab/test-1',
+            'aliases' => array(),
+            'reference' => '7ea49c8bbf6ae777e9899a7c9a7c08b1792dbce3',
+            'dev_requirement' => false,
+        ),
+        'kasyanovab/test-2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'prpject',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b3e4f3ad7361a44456524e2e6a7ec17e923ee1af',
+            'dev_requirement' => false,
+        ),
+    ),
+);
